5d0078ebc6efba326b5452fa907a07c92000fbc7
pranav6527 <pranav6527@gmail.com>
2022-09-10 18:59:52 +0530

    Initial project push

---
+++ bloodbank-backend/src/main/java/com/krishna/bloodbankbackend/BloodbankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.bloodbankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodbankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodbankBackendApplication.class, args);
+	}
+
+}
